0260c2678013f1bda89f8720564ced16aa2b4cef
Pate_Ball <roger0@gmx.de>
2020-10-31 19:03:19 +0100

    CSS geändert

--- nav.php
+++ nav.php
@@ -9,18 +9,18 @@ body {
 	background-color: #2e3339;
 	color: #fff;
 }
-.table-striped > tbody > tr:nth-of-type(2n+1) {
+.table-striped > tbody > tr:nth-of-type(2n+1)-dark {
     id="backgroundColorOne"; 
     background-color: #3f4143;
 }
-.table-striped > tbody > tr:nth-of-type(2n) {
+.table-striped > tbody > tr:nth-of-type(2n)-dark {
     background-color: #2e3339;
 }
-.navbar-default {
+.navbar-default-dark {
   background-color: #3f4143;
   border-color: #000;
 }
-.panel-default > .panel-heading {
+.panel-default > .panel-heading-dark {
   color: #fff;
   background-color: #3f4143;
   border-color: #000;
